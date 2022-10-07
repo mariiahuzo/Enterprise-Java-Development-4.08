57722258a95eb6faf19a7fa394e196fbd09d8bdb
mariiahuzo <d.mariya.d@gmail.com>
2022-10-07 23:59:52 +0200

    first commit

---
+++ src/main/java/com/ironhack/Lab_4_08/Lab_4_08/Lab408Application.java
@@ -0,0 +1,13 @@
+package com.ironhack.Lab_4_08.Lab_4_08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab408Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab408Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ironhack/Lab_4_08/Lab_4_08/Lab408ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.Lab_4_08.Lab_4_08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab408ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
